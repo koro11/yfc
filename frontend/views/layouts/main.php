6630d14be2393a82b65e8bf3fd8331a74759c0d3
e <xjx811811>
2017-02-27 10:37:57 +0800

    xiu

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -41,7 +41,8 @@
                     <a href="javascript:;" onClick="selectsearch(this,'restaurant_name')" <?php if(!isset($_GET['search_type']) || $_GET['search_type']!='food'){echo 'class="choose"';}?>>餐厅</a>
                     <a href="javascript:;" onClick="selectsearch(this,'food_name')" <?php if(isset($_GET['search_type']) && $_GET['search_type']=='food'){echo 'class="choose"';}?>>食物名</a>
                 </div>
-                <input type="hidden" name="search_type" value="<?php if(isset($_GET['search_type']) && $_GET['search_type']=='shop'){echo 'shop';}else{echo 'food';}?>">
+                <input type="hidden" name="search_type" value="<?php if(isset($_GET['search_type']) && !
+                    empty($_GET['search_type'])){echo $_GET['search_type'];}else{echo 'shop';}?>">
                 <div class="Search_area">
                     <input type="search" id="fkeyword" name="keyword" <?php if(isset($_GET['keyword']) && !empty($_GET['keyword'])){echo 'value="'.$_GET['keyword'].'"';}?> placeholder="请输入您所需查找的餐厅名称或食物名称..." class="searchbox"/>
                     <input type="submit" class="searchbutton" value="搜 索"/>
