5b000dfb85eb57a58bf2ac95adbc4f1f16f7ccac
danny <351518543@qq.com>
2017-03-15 11:09:54 +0800

    合并冲突提交311小改动之五

--- frontend/views/login/merchant_login.php
+++ frontend/views/login/merchant_login.php
@@ -35,7 +35,7 @@
 									<label><input name="" type="checkbox"> 下次自动登录</label>
 								</div>
 								<div class="right check_right">
-									<a href="#">忘记密码</a>
+									<a href="javascript:;">忘记密码</a>
 								</div>
 							</div>
 							<div class="form_btn">
